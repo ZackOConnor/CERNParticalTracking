6bb4f30a6ef160d9323876f35cb1019d3c23d59d
ZackOConnor <40275189+ZackOConnor@users.noreply.github.com>
2019-05-31 13:05:03 -0400

    Update data_loader.py
    added doc strings

--- data_loader.py
+++ data_loader.py
@@ -3,11 +3,12 @@ import glob
 
 
 def read_files(path):
+    """reads in and separates all the files with in the folder into a list"""
     return [p.split('-')[0] for p in sorted(glob.glob(path))]
 
 
 def load_data():
-    #Grabs the train, test, and submission folders
+    """Sets the train, test, and sub files from the files list created from read_files"""
     train = np.unique(read_files('./train_data/**.csv'))
     test = np.unique(read_files('./test_data/**.csv'))
     sub = np.unique(read_files('./sample_submission/**.csv'))
