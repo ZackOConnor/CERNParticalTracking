2deb7e8cef612c7b9cdb861e5e85eef03e53a9a2
ZackOConnor <40275189+ZackOConnor@users.noreply.github.com>
2019-05-31 13:02:17 -0400

    Update model.py
    adding doc string

--- model.py
+++ model.py
@@ -5,20 +5,24 @@ from keras.layers.embeddings import Embedding
 
 
 def make_model():
+    """Builds the LSTM, with 2 layers and reasonable drop out, 
+    and binary cross entropy as the loss function
+    """
     model = Sequential()
     model.add(Embedding(50000, 100, input_length=11))
+    #layer 1
     model.add(LSTM(256, return_sequences=True))
     model.add(Dropout(0.5))
     model.add(BatchNormalization())
+    #layer 2
     model.add(LSTM(128))
     model.add(Dropout(0.5))
     model.add(BatchNormalization())
     model.add(Dense(1, activation='sigmoid'))
-    #Builds the LSTM, with 2 layers and reasonable drop out
 
+    #Defines loss function and prediction metric
     model.compile(loss='binary_crossentropy',
                   optimizer='adam',
                   metrics=['accuracy'])
-    # Defines loss function and prediction metric
 
     return model
