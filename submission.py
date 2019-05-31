681416590de1f7ee280771847a2a2dcae19a5721
ZackOConnor <40275189+ZackOConnor@users.noreply.github.com>
2019-05-31 12:59:43 -0400

    Update submission.py
    Cleaning up comments and adding a doc string

--- submission.py
+++ submission.py
@@ -2,7 +2,13 @@ import pandas as pd
 
 
 def make_submission(df_test, sub):
+    """Merge the event_id and hit_id back on to the test with prediction data frame to create the submission data frame
+        params:
+        df_test -- test data frame with prediction apended on
+        sub     -- the submission file with out predictions
+    """
     df_test = pd.concat(df_test, ignore_index=True)
+    #merge test with prediction data frame onto the submissions data frame
     sub = pd.merge(sub, df_test, how='left', on=[
                    'event_id', 'hit_id'])
     sub['track_id'] = sub['particle_id'] + 1
