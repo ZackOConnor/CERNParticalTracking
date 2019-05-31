c3c49cfd7771351a1316e93427ec015626f6fd32
ZackOConnor <40275189+ZackOConnor@users.noreply.github.com>
2019-05-31 12:47:56 -0400

    Update test.py
    Cleaning up comments added doc string

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 from trackml.dataset import load_event
 import pandas as pd
 
-
 def test(model, test, shift=10000):
+    """create prediction from train model over the test data. then append the prediction to the test data tp create the submission
+    model   -- trained model
+    test    -- test data
+    shift   -- coordinate shift needed to eliminate negitive numbers
+    """
     df_test = []
 
     for e in test:
@@ -12,18 +16,17 @@ def test(model, test, shift=10000):
             'ch0', 'ch1', 'value'].agg(['mean']).reset_index()
         cells.columns = ['hit_id', 'ch0', 'ch1', 'value']
         hits = pd.merge(hits, cells, how='left', on='hit_id')
-        # col = [c for c in hits.columns if c not in ['event_id', 'hit_id', 'particle_id']]
-        hits = pd.merge(hits, truth, how='left', on='hit_id')
         # Pulls in all needed data from the diffrent folders on a event by even bases
+        hits = pd.merge(hits, truth, how='left', on='hit_id')
         print(e, "Test")
+        # the +10000 shifts the event space into a non-negitive space, the model won't take negitives
         hits['x'] = hits['x'] + shift
         hits['y'] = hits['y'] + shift
         hits['z'] = hits['z'] + shift
-        # the +10000 shifts the event space into a non-negitive space, the model won't take negitives
+        # Predicts the test set
         hits['particle_id'] = model.predict(
             hits[['hit_id', 'x', 'y', 'z', 'volume_id', 'layer_id', 'module_id', 'event_id', 'ch0', 'ch1', 'value']].values, verbose=1)
-        # Predicts the test set
-        df_test.append(hits[['event_id', 'hit_id', 'particle_id']].copy())
         # append the predictions onto the test data frame
+        df_test.append(hits[['event_id', 'hit_id', 'particle_id']].copy())
 
     return df_test
